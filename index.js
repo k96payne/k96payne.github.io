e5fb50c39cd63dd874192e8836f6b7a427bdc51d
Kyle Payne <kpayne96@ufl.edu>
2018-01-24 19:49:36 -0500

    edit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-  $fixTitle.classListAdd( "removeBackImage" );
+  document.querySelector( "#fixTitle" ).classListAdd( "removeBackImage" );
 }
 
 // Select all links with hashes
